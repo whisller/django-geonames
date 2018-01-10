21eb889c1f560cb701897ee97476ea6be143c441
Daniel Ancuta <whisller@gmail.com>
2018-01-10 21:08:13 +0000

    Define DB model.

--- django_geonames/browser/models.py
+++ django_geonames/browser/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    class Meta:
+        db_table = 'cities'
+
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    country_code = models.CharField(max_length=255)
